7a149814387b6783addbb9518c661ccfbb11b2be
Miniza <minenhledlamini37@gmail.com>
2022-06-14 01:23:24 +0200

    Delete user info when they delete profile

--- functions/src/createProfile.ts
+++ functions/src/createProfile.ts
@@ -5,7 +5,6 @@ import {db} from "./admin";
 const docRef = db.firestore().collection("users");
 export const createProfile = async (userRecord:any) => {
   const {email, displayName, uid} = userRecord;
-
   try {
     return await docRef.doc(uid).set({email, displayName, uid});
   } catch (message) {
@@ -13,7 +12,16 @@ export const createProfile = async (userRecord:any) => {
   }
 };
 
+export const deleteFirestoreUser = async (userRecord:any) => {
+  const {uid} = userRecord;
+  try {
+    return await docRef.doc(uid).delete();
+  } catch (message) {
+    return console.error(message);
+  }
+};
 
 module.exports = {
   authOnCreate: functions.auth.user().onCreate(createProfile),
+  deleteFirestoreuser: functions.auth.user().onDelete(deleteFirestoreUser),
 };
